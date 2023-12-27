a4b37845b8dbfa4b60a489fd78abc555cd2f30f8
Daniyal Khan <daniyalkhan131@gmail.com>
2023-12-27 21:31:44 +0530

    trying

--- train.py
+++ train.py
@@ -4,12 +4,12 @@ import yaml
 from dvclive import Live
 
 with Live(save_dvc_exp=True) as live:
-    #epochs = 10 #we are tracking for 10 epochs and saving records  for each epoch
+    epochs = 10 #we are tracking for 10 epochs and saving records  for each epoch
 
     #we can change params.yaml file in dvclive to control things, anf for manipulating .yaml use uaml lib
 
-    train_params=yaml.safe_load(open('params.yaml'))['train']
-    epochs=train_params['epochs ']
+    #train_params=yaml.safe_load(open('params.yaml'))['train']
+    #epochs=train_params['epochs ']
     live.log_param ("epochs", epochs)
     for epoch in range(epochs):
         live.log_metric("train/accuracy", epoch + random.random()) 
