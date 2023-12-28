af6972db0b2d14b595ee0af383e5c8be41b0e06c
Daniyal Khan <daniyalkhan131@gmail.com>
2023-12-28 20:05:25 +0530

    initial trying commits and mlflow some lines added

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import random
 import sys
 import yaml
 from dvclive import Live
+import mlflow
 
 with Live(save_dvc_exp=True) as live:
     epochs = 10 #we are tracking for 10 epochs and saving records  for each epoch
@@ -11,11 +12,18 @@ with Live(save_dvc_exp=True) as live:
     #train_params=yaml.safe_load(open('params.yaml'))['train']
     #epochs=train_params['epochs ']
     live.log_param ("epochs", epochs)
+    mlflow.log_param ("epochs", epochs)
     for epoch in range(epochs):
         live.log_metric("train/accuracy", epoch + random.random()) 
         live.log_metric("train/loss", epochs - epoch - random.random())
         live.log_metric("val/accuracy", epoch + random.random())
         live.log_metric("val/loss", epochs - epoch - random.random()) 
+
+        mlflow.log_metric("train/accuracy", epoch + random.random()) 
+        mlflow.log_metric("train/loss", epochs - epoch - random.random())
+        mlflow.log_metric("val/accuracy", epoch + random.random())
+        mlflow.log_metric("val/loss", epochs - epoch - random.random()) 
+        
         live.next_step()
 
 #some files are created using this like dvclive and in that we plot things and do
